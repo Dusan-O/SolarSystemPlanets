351059b37c14cdae63e8afe0de341d1e33f48502
Dusan-O <dusan.orescanin@gmail.com>
2022-08-30 22:16:52 +0200

    Initial Commit

---
+++ SolarSystemPlanets/SolarSystemPlanetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolarSystemPlanetsApp.swift
+//  SolarSystemPlanets
+//
+//  Created by Dusan Orescanin on 30/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct SolarSystemPlanetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
